cc16ea9617273dcb8b221b9fa42a0e5bd5adb042
Aram Kazorian <aramk@berkeley.edu>
2024-01-17 21:13:09 -0800

    changed file name, added reference image and beginning matching algo

--- main.py
+++ main.py
@@ -24,7 +24,16 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
+ref_image = cv.imread('solution.jpg', cv.IMREAD_GRAYSCALE)
+ref_image = np.asarray(ref_image, dtype="uint8") / 255.0
+ref_copy = ref_image
+
 img = cv.imread('./Pieces/IMG_8340.jpg', cv.IMREAD_GRAYSCALE)
+img = np.asarray(img, dtype="uint8") / 255.0
+
+h, w = ref_image.shape
+out_img = np.zeros((h, w))
+
 
 assert img is not None, "file could not be read, check with os.path.exists()"
 
@@ -32,7 +41,7 @@ ret, thresh = cv.threshold(img, 127, 255, 0)
 
 contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-cv.drawContours(img, contours, -1, (0,255,0), 3)
+cv.drawContours(img, contours, -1, (255,255,0), 2)
 
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
